1a50c2f33662f64f4e91a372459542b4af62b487
Anjelica(Anjie-MF) <anjiemay23@penguin>
2025-11-05 17:20:34 -0600

    feat: must add preventdefault modal

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// write ovveride modal or toast(?) thanking customer
+
+
+
+// User clicks submit
+// Your event listener catches it with preventDefault()
+// You send the form data to Formspree using fetch() or AJAX
+// You get the response back
+// You show your custom modal ✅ (you stay in control)
